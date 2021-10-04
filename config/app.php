d9980ae17cd1563e383e37e598cf95cd88a59ec0
DESKTOP-31L1003\Owner <sweetchild00008@gmail.com>
2021-10-04 16:33:34 +0800

    up

--- config/app.php
+++ config/app.php
@@ -12,6 +12,8 @@ return [
     | any other location as required by the application or its packages.
     |
     */
+    'apiCode' => env('APICODE'),
+    'apiPass' => env('APIPASS'),
 
     'name' => env('APP_NAME', 'Laravel'),
 

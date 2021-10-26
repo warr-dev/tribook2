152ea3f5d60479ced0477fba971b2f7d1e0c4e45
DESKTOP-31L1003\Owner <sweetchild00008@gmail.com>
2021-10-26 17:02:22 +0800

    fix location seeding

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -14,21 +14,21 @@ class LocationSeeder extends Seeder
      */
     public function run()
     {
-        Locations::truncate();
-        $locations=[
-            'Calapan Public Market'=>0,
-            'C5 Road Calapan'=>4.7,
-            'Masipit'=>3.7,
-            'Tawiran'=>2.3,
-            'Lalud'=>1.8,
-            'Camilmil'=>1,
-            'Sta Rosa Baco'=>19,
-        ];
-        foreach(array_keys($locations) as $key){
-            Locations::create([
-                'name'=>$key,
-                'distance'=>$locations[$key]
-            ]);
-        }
+        // Locations::truncate();
+        // $locations=[
+        //     'Calapan Public Market'=>0,
+        //     'C5 Road Calapan'=>4.7,
+        //     'Masipit'=>3.7,
+        //     'Tawiran'=>2.3,
+        //     'Lalud'=>1.8,
+        //     'Camilmil'=>1,
+        //     'Sta Rosa Baco'=>19,
+        // ];
+        // foreach(array_keys($locations) as $key){
+        //     Locations::create([
+        //         'name'=>$key,
+        //         'distance'=>$locations[$key]
+        //     ]);
+        // }
     }
 }
